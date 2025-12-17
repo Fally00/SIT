f15d50a4220eab009d78a7644bfa66909974faf4
Rayo <rayanhisgam1234@gmail.com>
2025-12-17 22:48:30 +0200

    System Info header is done

---
+++ systemInfo.h
@@ -0,0 +1,22 @@
+#pragma once 
+#include <string>
+
+namespace SystemInfo {
+    // Returns the operating system name
+    std::string getOSName();
+
+    //Returns the CPU model name
+    std::string getCPUModel();
+
+    // Returns the ram size in GB
+    std::string getRam();
+
+    // Returns the total physical disk size in GB
+    uint64_t getDisk();
+
+    // Returns the uptime in seconds
+    int getUptime();
+
+    //Returns the current user name
+    std::string getUserName();
+} 
